91181e29abae9579b66aed7646975c3505d45801
gavincoding <gavinroak03@gmail.com>
2024-11-18 16:33:35 -0500

    added First person view. Click Space to switch

--- Frontend/Assets/camMover.cs
+++ Frontend/Assets/camMover.cs
@@ -6,7 +6,18 @@ public class NoClipCam : MonoBehaviour
     [SerializeField] private float ascendDescendSpeed = 5f;
     [SerializeField] private float mouseSensitivity = 2f;
 
+    public GameObject drone;
+
     private Vector3 moveDirection;
+
+    private float followX;
+    private float followY;
+    private float followZ;
+    private float followRoll;
+    private float followPitch;
+    private float followYaw;
+
+    bool isFollowing = false;
 
     void Start()
     {
@@ -16,10 +27,38 @@ public class NoClipCam : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isFollowing = !isFollowing;
+        }
+        if(isFollowing)
+        {
+            followDrone();
+        }
+        else
+        {
+            noClip();
+        }
+    
+        
+    }
+    
+    private void followDrone(){
+        followX = drone.transform.position.x;
+        followY = drone.transform.position.y;
+        followZ = drone.transform.position.z;
+
+        followRoll = drone.transform.rotation.eulerAngles.x;
+        followPitch = drone.transform.rotation.eulerAngles.y;
+        followYaw = drone.transform.rotation.eulerAngles.z;
+
+        transform.rotation = Quaternion.Euler(followRoll, followPitch, followYaw);
+        transform.position = new Vector3(followX, followY, followZ);
+    }
+    private void noClip(){
         // Get WASD input for movement along the XZ plane
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
-        
         // Calculate forward/backward/left/right movement
         moveDirection = (transform.right * moveX + transform.forward * moveZ) * moveSpeed;
 
