d7df400ae9330695f520b5f70e574e0270dcaf63
Abhinav-Kotta <abhinav.kotta@gmail.com>
2025-01-06 20:06:59 -0500

    testing

--- DatabaseConnectionExample.py
+++ DatabaseConnectionExample.py
@@ -1,12 +1,24 @@
 import psycopg2
 
 #Connection string
-CONNECTION = "your-connection-string"
+CONNECTION = "postgres://tsdbadmin:r11z8rd99hapocaq@q7pcmp9w1b.fi3yauzvon.tsdb.cloud.timescale.com:34181/tsdb?sslmode=require"
 
 def get_all_data():
     conn = psycopg2.connect(CONNECTION)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM sensors")
-    rows = cursor.fetchall()
+    
+    cursor.execute("""
+        SELECT * FROM pg_stat_statements
+    """)
+    
+    tables = cursor.fetchall()
+    
+    # Print the tables
+    print("\nTables in database:")
+    for table in tables:
+        print(table[0])  # table[0] since fetchall returns tuples
+        
     cursor.close()
-    return rows
+    conn.close()
+
+get_all_data()
