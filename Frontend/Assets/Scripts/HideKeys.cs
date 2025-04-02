2aa7f737207b9f99dc4730cc85bb83dff4de1bc1
GavinCoding <gavinroak03@gmail.com>
2025-04-02 05:21:42 -0400

    keyboards

--- Frontend/Assets/Scripts/HideKeys.cs
+++ Frontend/Assets/Scripts/HideKeys.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 public class ClickSelect : MonoBehaviour{
     [SerializeField] private GameObject UserKeyboard; // Reference to the XRKeyboard
     [SerializeField] private GameObject PassKeyboard;
+    [SerializeField] private GameObject SecurityQKeyboard; // Reference to the XRKeyboard for security questions
+    [SerializeField] private GameObject SecurityAKeyboard; // Reference to the XRKeyboard for security answers
     public void ShowKeysUser(){
         // This method will be called when the object is clicked
         // It will hide the XRKeyboard
@@ -48,6 +50,54 @@ public class ClickSelect : MonoBehaviour{
         if (PassKeyboard != null)
         {
             PassKeyboard.SetActive(false); // Hide the keyboard
+        }
+        else
+        {
+            Debug.LogError("XRKeyboard reference not set.");
+        }
+    }
+    public void ShowKeysSecurityQ(){
+        // This method will be called when the object is clicked
+        // It will hide the XRKeyboard
+        if (SecurityQKeyboard != null)
+        {
+            SecurityQKeyboard.SetActive(true); // Show the keyboard
+        }
+        else
+        {
+            Debug.LogError("XRKeyboard reference not set.");
+        }
+    }
+    public void HideKeysSecurityQ(){
+        // This method will be called when the object is clicked
+        // It will hide the XRKeyboard
+        if (SecurityQKeyboard != null)
+        {
+            SecurityQKeyboard.SetActive(false); // Hide the keyboard
+        }
+        else
+        {
+            Debug.LogError("XRKeyboard reference not set.");
+        }
+    }
+    public void ShowKeysSecurityA(){
+        // This method will be called when the object is clicked
+        // It will hide the XRKeyboard
+        if (SecurityAKeyboard != null)
+        {
+            SecurityAKeyboard.SetActive(true); // Show the keyboard
+        }
+        else
+        {
+            Debug.LogError("XRKeyboard reference not set.");
+        }
+    }
+    public void HideKeysSecurityA(){
+        // This method will be called when the object is clicked
+        // It will hide the XRKeyboard
+        if (SecurityAKeyboard != null)
+        {
+            SecurityAKeyboard.SetActive(false); // Hide the keyboard
         }
         else
         {
